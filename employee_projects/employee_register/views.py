49639b2f7ed97acf65b2f5c7e81336585f3afd49
Gaspela <samirmillanorozco@hotmail.com>
2021-11-15 17:35:56 -0500

    Add structutre app

---
+++ employee_projects/employee_register/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def employee_List(request):
+    return
+
+def employee_Form(request):
+    return
+
+def employee_Delete(request):
+    return
