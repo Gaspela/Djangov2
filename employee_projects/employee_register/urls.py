a04c63af90dfdc354532508b4618a94945d04834
Gaspela <samirmillanorozco@hotmail.com>
2021-11-22 10:53:06 -0500

    Base html

--- employee_projects/employee_register/urls.py
+++ employee_projects/employee_register/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.employee_Form),
     path('list/', views.employee_List),
-    path('delete/', views.employee_Delete)
 
 ]
